e8d255402d48218f5f8e6bc3be72aa046ee37631
esafadhila <e.noerf@gmail.com>
2021-02-26 10:07:55 +0700

    Esa 1908517

---
+++ aktifitas4.py
@@ -0,0 +1,10 @@
+print("Contoh perulangan suatu string")
+for huruf in "SISTEL":
+    print(huruf)
+    
+nilai_awal=0
+nilai_akhir=25 #tujuan/berapa kali
+step=5 #jeda antar loop
+print("Contoh perulangan angka dengan range()")
+for i in range(nilai_awal, nilai_akhir, step):
+    print(i)
