e5508b7d10e91b5341f9d8c970c722ed9799e36b
esafadhila <e.noerf@gmail.com>
2021-02-26 09:13:14 +0700

    Esa 1908527

---
+++ aktifitas1.py
@@ -0,0 +1,11 @@
+#INPUT
+
+nama=input("Masukkan Nama Anda: ")
+umur=19
+
+#OUTPUT
+print("")
+print("=====INPUT DAN OUTPUT PADA BAHA PEMOGRAMAN PYTHON=====")
+print("Nama Anda: ", nama)
+print("Umur Anda: ", umur)
+print("")
